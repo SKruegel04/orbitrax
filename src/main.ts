f31c40f292c6c40997d85030cc90fdf7b2dedaf5
Simone Krügel <simone.kruegel04@outlook.de>
2024-07-05 01:10:13 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+type Orbit = {
+    semiMajorAxis: number
+    eccentricity: number
+    inclination:  number
+    raan: number
+    argOfPeriapsis: number
+    meanAnomaly: number
+}
+
+const exampleOrbit: Orbit = {
+    semiMajorAxis: 7000,
+    eccentricity: 0.001,
+    inclination: 98.7,
+    raan: 180,
+    argOfPeriapsis: 270,
+    meanAnomaly: 0
+}
